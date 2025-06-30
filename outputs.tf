a7c896efcb371cb843cfd2daafed99a8769cb775
Enno Richter <enno@nerdworks.de>
2025-06-30 15:41:08 +0200

    expose lb ips

--- outputs.tf
+++ outputs.tf
@@ -4,6 +4,16 @@ output "kubeconfig_data" {
   sensitive   = true
 }
 
+output "ingress_service_load_balancer_public_ipv4" {
+  description = "Public IPv4 address of the main ingress load balancer"
+  value       = module.k8s.ingress_service_load_balancer_public_ipv4
+}
+
+output "ingress_service_load_balancer_public_ipv6" {
+  description = "Public IPv6 address of the main ingress load balancer"
+  value       = module.k8s.ingress_service_load_balancer_public_ipv6
+}
+
 output "argo_workflows_managed_namespaces" {
   description = "List of namespaces where workflows will be managed by Argo Workflows."
   value       = var.argo_workflows_managed_namespaces
